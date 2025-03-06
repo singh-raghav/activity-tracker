ed767cc809438c6d550e02b199cb639c914766b9
Raghav Singh <raghavsingh@Raghavs-MacBook-Air.local>
2025-03-06 18:47:02 +0530

    initial setup

---
+++ src/test/java/com/activity/tracker/ActivityTrackerApplicationTests.java
@@ -0,0 +1,17 @@
+
+package com.activity.tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+
+
+/* todo This needs to be fixed */
